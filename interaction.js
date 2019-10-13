b0ea6a207fa16babbbfdc927b4c6c725d48f7551
Luis Brito <nottluis@gmail.com>
2019-10-13 19:05:45 -0300

    Updated API url

--- interaction.js
+++ interaction.js
@@ -4,7 +4,7 @@ $(function () {
     $.ajaxSetup({ cache: false });
     $(".message");
     $.getJSON(
-      "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&callback=",
+      "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json",
       function (data) {
         let random = Math.floor(Math.random() * data.quotes.length);
         $(".message")
